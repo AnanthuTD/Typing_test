aecbf8028e30c68c172ee4ea874e21e375fa26d8
AnanthuTD <ananthutd2021@gmail.com>
2023-03-27 21:38:23 +0530

    wpm added

--- pages/game.jsx
+++ pages/game.jsx
@@ -5,8 +5,13 @@ import { useState, useEffect } from "react";
 function game() {
     const [passage, setPassage] = useState("");
     const [converted, setConverted] = useState();
+    let correctLetterCount = 0;
+    // total time in seconds
+    const [totalTime, setTotalTime] = useState(0);
+    const [wpm, setWpm] = useState(0);
 
     useEffect(() => {
+        let time = 0;
         var index = Math.floor(Math.random() * Text.length);
         setPassage(Text[index]);
         let str = [];
@@ -26,6 +31,7 @@ function game() {
                 );
         }
         setConverted(str);
+        let firstKeyDown = true;
         let count = 0;
         const keyDownHandler = (e) => {
             if (e.key == "Backspace") {
@@ -34,7 +40,15 @@ function game() {
                 p_tag.style.backgroundColor = "transparent";
                 return;
             }
-            console.log(`You pressed ${e.key}.`);
+            /*  let char_code = e.key.charCodeAt(0)
+            if (
+                (char_code < 65 ||char_code > 90) &&
+                (char_code < 97 ||char_code > 122) &&
+                (char_code < 48 || char_code > 57) 
+            ) {
+                return;
+            }
+            console.log(e.key.charCodeAt(0)); */
             if (
                 e.key == "Shift" ||
                 e.key === "CapsLock" ||
@@ -43,27 +57,32 @@ function game() {
             ) {
                 return;
             }
-            if (
-                (e.key < "A" || e.key > "Z") &&
-                (e.key < "a" || e.key > "z") &&
-                (e.key < "0" || e.key > "9")
-            ) {
-                return;
-            }
 
+            if (firstKeyDown) {
+                setInterval(() => {
+                    time = time + 1;
+                    setTotalTime(time);
+                    let letterPerSec = correctLetterCount / time;
+                    let secPerWord = 5 / letterPerSec;
+                    let wordPerMin = parseInt(60 / secPerWord);
+                    setWpm(wordPerMin);
+                }, 1000);
+            }
+            firstKeyDown = false;
             let char = e.key;
 
             if (passage[count] != char) {
-                console.log("count = ", count);
                 let p_tag = document.getElementById(`passage_${count}`);
-                p_tag.style.backgroundColor = "#f66";}
-                else{
-                    let p_tag = document.getElementById(`passage_${count}`);
+                p_tag.style.backgroundColor = "#f66";
+            } else {
+                let p_tag = document.getElementById(`passage_${count}`);
                 p_tag.style.backgroundColor = "rgb(163, 238, 175)";
-                }
+
+                correctLetterCount++;
+            }
             count++;
-        }
-       
+        };
+
         document.body.focus();
         document.addEventListener("keydown", keyDownHandler);
         return () => {
@@ -78,6 +97,8 @@ function game() {
                 <section className={styles.main}>
                     <div className={styles.outer_game}>{converted}</div>
                 </section>
+                <h1 style={{ color: "red" }}>{totalTime}</h1>
+                <h1 style={{ color: "yellow" }}>{wpm}</h1>
             </section>
         </>
     );
