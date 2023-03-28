bcb8841bd627aaec4792c41b4ae04cf676b8406e
AnanthuTD <ananthutd2021@gmail.com>
2023-03-28 21:55:19 +0530

    comment & color transition.

--- pages/game.jsx
+++ pages/game.jsx
@@ -3,40 +3,88 @@ import Text from "../public/text";
 import styles from "../styles/game.module.css";
 import { useState, useEffect } from "react";
 function game() {
+    // to store current passage.
+
     const [passage, setPassage] = useState("");
-    const [converted, setConverted] = useState();
-    let correctLetterCount = 0;
-    // total time in seconds
+
+    // to store a modified version of passage which is converted into jsx.
+
+    const [converted, setConverted] = useState([]);
+    /* if passage = "abc g"
+            converted[0] = <div>
+                            <p>a</p>
+                            <p>b</p>
+                            <p>c</p>
+                        </div>
+            converted[1] = <p>&nbsp;</p>
+            converted[2] = <div><p>g</p></div>
+    */
     const [totalTime, setTotalTime] = useState(0);
     const [wpm, setWpm] = useState(0);
 
     useEffect(() => {
-        let time = 0;
-        var index = Math.floor(Math.random() * Text.length);
+        let correctLetterCount = 0,
+            time = 0;
+
+        // selecting random passage
+
+        let index = Math.floor(Math.random() * Text.length);
         setPassage(Text[index]);
-        let str = [],
-            word = 0;
-        let temp = [];
+
+        // converting passage into jsx (<div><p>single letter</p></div>) format.
+
+        let convertedPassage = []; // array to store modified passage.
+
+        let temp = []; // to store each letter of a word, which is in a p tag.
+
+        let wordIndex = 0; // to track where to insert next word.
+
+        // iterating through each letter of the passage
+
         for (let index = 0; index < passage.length; index++) {
-            let element = passage[index];
-            if (element !== " ") {
+            let letter = passage[index];
+
+            // if letter is not a space insert it make a p tag with it
+
+            if (letter !== " ") {
                 temp[index] = (
                     <p key={`passage_${index}`} id={`passage_${index}`}>
-                        {element}
+                        {letter}
                     </p>
                 );
             } else {
-                str[word++] = <div key={`word_${word}`} className="d-flex">{temp}</div>;
-                str[word++] = (
+                // if space, insert all the p tag to a div and store it in convertedPassage.
+
+                convertedPassage[wordIndex++] = (
+                    <div key={`word_${wordIndex}`} className="d-flex">
+                        {temp}
+                    </div>
+                );
+
+                // add space after each word
+
+                convertedPassage[wordIndex++] = (
                     <p key={`passage_${index}`} id={`passage_${index}`}>
                         &nbsp;
                     </p>
                 );
+
+                // clearing contents of temp.
+
                 temp = [];
             }
         }
-        str[word] = <div key={`word_${word}`} className="d-flex">{temp}</div>;
-        setConverted(str);
+
+        // inserting last word into convertedPassage, since there is no space after the last word.
+
+        convertedPassage[wordIndex] = (
+            <div key={`word_${wordIndex}`} className="d-flex">
+                {temp}
+            </div>
+        );
+
+        setConverted(convertedPassage);
+
         let firstKeyDown = true;
         let count = 0;
         const keyDownHandler = (e) => {
@@ -46,15 +94,9 @@ function game() {
                 p_tag.style.color = "white";
                 return;
             }
-            /*  let char_code = e.key.charCodeAt(0)
-            if (
-                (char_code < 65 ||char_code > 90) &&
-                (char_code < 97 ||char_code > 122) &&
-                (char_code < 48 || char_code > 57) 
-            ) {
-                return;
-            }
-            console.log(e.key.charCodeAt(0)); */
+
+            // skip these keys
+
             if (
                 e.key == "Shift" ||
                 e.key === "CapsLock" ||
@@ -64,19 +106,56 @@ function game() {
                 return;
             }
 
+            // on first keyDown start counting time in sec and calculate wpm(words per minute).
+
             if (firstKeyDown) {
-                setInterval(() => {
-                    time = time + 1;
-                    setTotalTime(time);
-                    let letterPerSec = correctLetterCount / time;
-                    let secPerWord = 5 / letterPerSec;
-                    let wordPerMin = parseInt(60 / secPerWord);
-                    setWpm(wordPerMin);
-                }, 1000);
+                setInterval(
+                    /* this function will be called on each second */
+                    () => {
+                        time = time + 1;
+                        setTotalTime(time);
+
+                        // letters typed per second.
+
+                        let letterPerSec = correctLetterCount / time;
+
+                        /* The "word" is an average of 5 characters.
+                           To calculate WPM, simply take the number of words typed in a minute
+                           with no typos and divide by five. For example, if you type 100 words
+                           in a minute including spaces, your typing speed would be 20 WPM.
+                           https://www.ratatype.com/typing-test/#:~:text=The%20most%20common,be%2020%20WPM.
+                        */
+                       
+                        //    5 letters == a word
+
+                        let secPerWord = 5 / letterPerSec;
+                        let wordPerMin = parseInt(60 / secPerWord);
+                        if (wordPerMin > 7) {
+                            let bgTransition = document.getElementById("main_game")
+                            bgTransition.style.backgroundColor = "yellow"
+                        }
+                        if (wordPerMin > 13) {
+                            let bgTransition = document.getElementById("main_game")
+                            bgTransition.style.backgroundColor = "green"
+                        }
+                        if (wordPerMin > 22) {
+                            let bgTransition = document.getElementById("main_game")
+                            bgTransition.style.backgroundColor = "orange"
+                        }
+                        if (wordPerMin > 30) {
+                            let bgTransition = document.getElementById("main_game")
+                            bgTransition.style.backgroundColor = "red"
+                        }
+                        setWpm(wordPerMin);
+                    },
+                    1000 // 1000 ms == 1 s.
+                );
             }
             firstKeyDown = false;
-            let char = e.key;
 
+            // color change on each correct and wrong letter.
+
+            let char = e.key;
             if (passage[count] != char) {
                 let p_tag = document.getElementById(`passage_${count}`);
                 if (passage[count] == " ") {
@@ -92,8 +171,16 @@ function game() {
             count++;
         };
 
+        // focus on body by default(to start typing without clicking on the page).
+
         document.body.focus();
+
+        // calling keyDownHandler (named function) on keydown
+
         document.addEventListener("keydown", keyDownHandler);
+            
+            
+
         return () => {
             document.removeEventListener("keydown", keyDownHandler);
             setConverted(null);
@@ -103,15 +190,24 @@ function game() {
     return (
         <>
             <section className={styles.body}>
-                <section className={`${styles.main} row`}>
+                <section className={`${styles.main}`} id="main_game">
                     <div
                         className={[
-                            styles.outer_game,
-                            "col-8",
+                            styles.passage,
+                            
                             "text-wrap",
                         ].join(" ")}
                     >
                         {converted}
+                    </div>
+                    <div
+                        className={[
+                            styles.wpm,
+                           
+                            "text-wrap",
+                        ].join(" ")}
+                    >
+                        <span>{wpm} wpm</span>
                     </div>
                 </section>
             </section>
